9833417bafbe16c7b396e3ef548761b3657640de
Leonardo Losoviz <leo@getpop.org>
2020-06-12 09:23:08 +0800

    Added dependency to migrate repo

--- src/Component.php
+++ src/Component.php
@@ -35,6 +35,13 @@ class Component extends AbstractComponent
         ];
     }
 
+    public static function getDependedMigrationPlugins(): array
+    {
+        return [
+            'migrate-locations',
+        ];
+    }
+
     /**
      * Initialize services
      */
