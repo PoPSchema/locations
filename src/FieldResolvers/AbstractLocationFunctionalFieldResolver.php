d6f2e07c8c7280a0880f4467bf8f76453b5da549
Leonardo Losoviz <leo@getpop.org>
2020-09-02 12:43:44 +0800

    Added object type to $resultItem in `resolveValue`

--- src/FieldResolvers/AbstractLocationFunctionalFieldResolver.php
+++ src/FieldResolvers/AbstractLocationFunctionalFieldResolver.php
@@ -42,8 +42,22 @@ abstract class AbstractLocationFunctionalFieldResolver extends AbstractFunctiona
         return $descriptions[$fieldName] ?? parent::getSchemaFieldDescription($typeResolver, $fieldName);
     }
 
-    public function resolveValue(TypeResolverInterface $typeResolver, $resultItem, string $fieldName, array $fieldArgs = [], ?array $variables = null, ?array $expressions = null, array $options = [])
-    {
+    /**
+     * @param array<string, mixed> $fieldArgs
+     * @param array<string, mixed>|null $variables
+     * @param array<string, mixed>|null $expressions
+     * @param array<string, mixed> $options
+     * @return mixed
+     */
+    public function resolveValue(
+        TypeResolverInterface $typeResolver,
+        object $resultItem,
+        string $fieldName,
+        array $fieldArgs = [],
+        ?array $variables = null,
+        ?array $expressions = null,
+        array $options = []
+    ) {
         switch ($fieldName) {
             case 'locationsmapURL':
                 $locations = $typeResolver->resolveValue($resultItem, 'locations', $variables, $expressions, $options);
